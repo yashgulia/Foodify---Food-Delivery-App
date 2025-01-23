938271dda5ef744dde688243472ee9a26b98453e
yashgulia <yashgulia09@gmail.com>
2025-01-23 23:08:06 +0530

    Changing Folder Structure

--- src/components/About/About.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const About = () => {
-  return <div className="mt-32">About</div>;
-};
-
-export default About;
--- src/components/Instamart/Instamart.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Instamart = () => {
-  return <div className="mt-32">Instamart</div>;
-};
-
-export default Instamart;
