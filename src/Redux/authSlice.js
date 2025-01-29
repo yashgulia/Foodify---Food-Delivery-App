41503f19b76edf97b6a41f7b960294c23bd19f15
yashgulia <yashgulia09@gmail.com>
2025-01-29 15:20:42 +0530

    Create authSlice.js

---
+++ src/Redux/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isAuthenticated: !!localStorage.getItem("token"),
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state) => {
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      localStorage.removeItem("token");
+    },
+  },
+});
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
